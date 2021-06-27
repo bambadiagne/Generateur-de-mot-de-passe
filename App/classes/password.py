4dff6c6ebfb2a522de3f9de80512e7deab5a4527
Ahmadou Bamba Diagne <diagne.bambahmadou@gmail.com>
2021-06-27 01:46:50 +0000

    Add action for alphanum password

--- App/classes/password.py
+++ App/classes/password.py
@@ -1,12 +1,12 @@
 from random import sample
 ALL_TYPES={'numeric':list('0123456789'),'alphalower':[chr(i) for i in range(97,123)],'alphaupper':[chr(i) for i in range(65,91)],'specialschars':['[', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', ',', '.', '<', '>', '/', '?', '|', ']'],'alphanum':1}
-  
+ALL_TYPES['alphanum']=ALL_TYPES['numeric']+ALL_TYPES['alphaupper']+ALL_TYPES['specialschars']+ALL_TYPES['alphalower']  
 class Password:
     def __init__(self, password_type, password_length=8):
+        
         self.password_length = password_length
         self.password_type = password_type
+    
     def gen_password(self,tabMotif):
-        if(self.password_type=='alphanum'):
-            ALL_TYPES['alphanum']=ALL_TYPES['numeric']+ALL_TYPES['alphaupper']+ALL_TYPES['specialschars']+ALL_TYPES['alphalower']
-        
-        return ''.join(sample(tabMotif,int(self.password_length)))
+            
+        return ''.join(sample([elt for type_password in tabMotif for elt in  ALL_TYPES[type_password]],int(self.password_length)))
--- classes/a.py
+++
@@ -1,4 +0,0 @@
-import random
-
-for i in range(10):
-    print(random.choice(list(range(10))))
