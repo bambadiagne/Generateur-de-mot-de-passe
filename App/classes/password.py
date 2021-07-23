d3e9aa964559c842f29990bed082d3517ab58db3
Ahmadou Bamba Diagne <diagne.bambahmadou@gmail.com>
2021-07-23 20:48:11 +0000

    Update classes

--- App/classes/password.py
+++ App/classes/password.py
@@ -1,12 +1,23 @@
-from random import sample
-ALL_TYPES={'numeric':list('0123456789'),'alphalower':[chr(i) for i in range(97,123)],'alphaupper':[chr(i) for i in range(65,91)],'specialschars':['[', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', ',', '.', '<', '>', '/', '?', '|', ']'],'alphanum':1}
-ALL_TYPES['alphanum']=ALL_TYPES['numeric']+ALL_TYPES['alphaupper']+ALL_TYPES['specialschars']+ALL_TYPES['alphalower']  
+from random import sample, choice, shuffle
+ALL_TYPES = {'numeric': list('0123456789'), 'alphalower': [chr(i) for i in range(97, 123)], 'alphaupper': [chr(i) for i in range(65, 91)], 'specialschars': [
+    '[', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', ',', '.', '<', '>', '/', '?', '|', ']'], 'alphanum': 1}
+
+
 class Password:
     def __init__(self, password_type, password_length=8):
-        
+
         self.password_length = password_length
         self.password_type = password_type
-    
-    def gen_password(self,tabMotif):
-            
-        return ''.join(sample([elt for type_password in tabMotif for elt in  ALL_TYPES[type_password]],int(self.password_length)))
+
+    def gen_password(self, tabMotif):
+        generated_password = self.randomPassword(tabMotif)
+        while not all(any(letter in ALL_TYPES[motif] for letter in generated_password) for motif in tabMotif):
+            generated_password = self.randomPassword(tabMotif)
+        return generated_password
+
+    def randomPassword(self, tabMotif):
+        passTab = list(choice([elt for type_password in tabMotif for elt in ALL_TYPES[type_password]])
+                       for i in range(int(self.password_length)))
+        shuffle(passTab)
+        print(passTab)
+        return ''.join(passTab)
